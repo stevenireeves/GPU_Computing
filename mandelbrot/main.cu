350af1f721783db8fab9b99c7131f3f7951c72e1
stevenireeves <reevessteven15@gmail.com>
2020-04-10 18:10:28 -0700

    C++ing stuff

--- mandelbrot/main.cu
+++ mandelbrot/main.cu
@@ -13,9 +13,9 @@ extern "C" {
 
 /* Mandlebrot rendering function
    :inputs: width and height of domain, max_iterations
-   :ouputs: 8bit character array containing mandlebrot image
+   :ouputs: 32bit character array containing mandlebrot image
 */
-__global__ void render(char out[], const int width, const int height, const int max_iter) {
+__global__ void render(unsigned int out[], const int width, const int height, const int max_iter) {
 
   // indexing for mandlebrot set, span domain for escape algo 
   int x_dim = blockIdx.x*blockDim.x + threadIdx.x;
@@ -23,16 +23,17 @@ __global__ void render(char out[], const int width, const int height, const int 
   // global index since we're creating a 3 channel image 
   int index = 3*(width*y_dim + x_dim);
 
-  if(index > 3*width*height) return; 
+  if(index >= 3*width*height) return; 
 
-  float x_origin = ((float) x_dim/width)*3.25 - 2;
-  float y_origin = ((float) y_dim/width)*2.5 - 1.25;
+  float x_origin = ((float) x_dim/width)*3.25 - 2; // "Real(C)"  C_x
+  float y_origin = ((float) y_dim/width)*2.5 - 1.25; // "Imaginary(C)" C_y  
 
   float x = 0.0;
   float y = 0.0;
 
   int iteration = 0;
   //escape algorithm
+  // Everythread will loop in this at most max_iter 
   while(x*x + y*y <= 4 && iteration < max_iter) {
     float xtemp = x*x - y*y + x_origin;
     y = 2*x*y + y_origin;
@@ -50,6 +51,12 @@ __global__ void render(char out[], const int width, const int height, const int 
     out[index + 2] = iteration;
   }
 }
+/*
+	Conditional loop in the middle -> threads will finish at different times
+	Thread divergence the program as fast as the slowest thread.   
+*/
+
+
 
 
 /* Host function for generating the mandlebrot image
@@ -57,25 +64,30 @@ __global__ void render(char out[], const int width, const int height, const int 
    :outputs: none
    writes a bmp image to disc
 */
-void mandelbrot(int width, int height, int max_iter)
+void mandelbrot(const int width, const int height, const int max_iter)
 {
 	// Multiply by 3 here, since we need red, green and blue for each pixel
   size_t buffer_size = sizeof(char) * width * height * 3;
 
-  char *image;
+  char *image; 
   cudaMalloc((void **) &image, buffer_size);
 
   char *host_image; 
-  host_image = new char[buffer_size]; 
+  host_image = new char[width*height*3]; 
 
-  dim3 blockDim(16, 16, 1);
-  dim3 gridDim(width / blockDim.x, height / blockDim.y, 1);
-  render<<< gridDim, blockDim, 0 >>>(image, width, height, max_iter);
+  dim3 block_Dim(16, 16, 1); // 16*16 threads 
+  dim3 grid_Dim(width / block_Dim.x, height / block_Dim.y, 1); //Rest of the image
 
+  /* dim3  int x, int y, int z */
+
+  /*kernel<< grid_Dim, block_Dim, #bytes_shared_mem>>> */ 
+  render<<< grid_Dim, block_Dim >>>(image, width, height, max_iter);
+
+  /*after render is done */ 
   cudaMemcpy(host_image, image, buffer_size, cudaMemcpyDeviceToHost);
 
   // Now write the file
-  write_bmp("output.bmp", width, height, host_image);
+  write_bmp("output_2.bmp", width, height, host_image);
 
   cudaFree(image);
   delete host_image;
@@ -85,6 +97,6 @@ void mandelbrot(int width, int height, int max_iter)
 /*main function */ 
 int main() 
 {
-  mandelbrot(7680, 7680, 8192);
+  mandelbrot(7680, 7680, 256);
   return 0;
 }
--- reversal/reversal.cu
+++ reversal/reversal.cu
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
+
 
 __global__ void staticReverse(int *d, int n)
 {
@@ -30,21 +32,20 @@ __global__ void dynamicReverse(int *d, int n)
 int main()
 {
 	const int n = 64; 
-	int *a, *d_a; 
-	a = (int*)malloc(n*sizeof(int));
+    std::vector<int> a(n,0); 
+	int *d_a;
 	cudaMalloc(&d_a, n*sizeof(int)); 
 
 	for(int i =0; i < n; i++)
 		a[i] = i; 
 	
-	cudaMemcpy(d_a, a, n*sizeof(int), cudaMemcpyHostToDevice); // Transfer to device
+	cudaMemcpy(d_a, a.data(), n*sizeof(int), cudaMemcpyHostToDevice); // Transfer to device
 
 	dynamicReverse<<<1, n, n*sizeof(int)>>>(d_a, n); //grid ,block ,shared
 	
-	cudaMemcpy(a,d_a, n*sizeof(int), cudaMemcpyDeviceToHost); //bring it back
+	cudaMemcpy(a.data(),d_a, n*sizeof(int), cudaMemcpyDeviceToHost); //bring it back
 	
 	std::cout<<a[0]<<std::endl;
-	free(a);
 	cudaFree(d_a);
 	return 0;
 }
