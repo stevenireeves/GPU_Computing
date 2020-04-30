4e9eae65de4a811b5048a39bd466cec32df1321e
stevenireeves <reevessteven15@gmail.com>
2020-04-29 18:33:52 -0700

    Re-inlined the device functions

--- matmul/submat.h
+++ matmul/submat.h
@@ -23,14 +23,16 @@ class subMatrix{
         // memory at spot
         elements = &A.elements[stride * width * row + height * col];
      }
-
 //Get matrix element
-    __device__ 
-    float GetElem(const int row, const int col);
-
-
-//Set a matrix element
-    __device__ 
-    void SetElem(const int row, const int col, const float value);
+	__device__ inline float GetElem(const int row, const int col)
+		{	
+	    return elements[row*stride + col];
+	} 
+	
+	//Set a matrix element
+	__device__ inline void SetElem(const int row, const int col, const float value)
+	{
+	     elements[row * stride + col] = value;
+	}
 };
 #endif
