f05d8a836f7df82a58bd1158b8c2a542cb870716
Steven I Reeves <reevessteven15@gmail.com>
2022-06-02 12:22:35 -0700

    Changed dpi.

--- hip/nbody/animate.py
+++ hip/nbody/animate.py
@@ -18,18 +18,12 @@ def update_graph(iterator):
     graph._offsets3d = (data[:,0,iterator], data[:,1,iterator], data[:,2, iterator])
 
 fig = plt.figure()
+fig.set_size_inches(5, 5)
 ax = fig.add_subplot(111, projection='3d')
 graph = ax.scatter(temp[:,0],temp[:,1],temp[:,2],s=temp[:,3]*10,marker='o')
 ani = animation.FuncAnimation(fig, update_graph, len(direc)-1, interval=50, blit=False, repeat=True)
 #plt.show()
+dpi = 300
 Writer = animation.writers['ffmpeg']
 writer = Writer(fps=60, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
-ani.save('N-Body.mp4', writer=writer)
-quit() 
-for k in range(len(direc)):
-	ax.scatter(data[:,0,k],data[:,1,k],data[:,2,k],s=data[:,3,k]*10,marker='o')
-	ax.set_xticks([])
-	ax.set_yticks([])
-	ax.set_zticks([])
-
-plt.show()
+ani.save('N-Body.mp4', writer=writer, dpi=dpi)
