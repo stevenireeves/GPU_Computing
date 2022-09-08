0d497c411e8431958cf36722fc6fd7d5dcd1ac01
Steven I Reeves <reevessteven15@gmail.com>
2022-09-08 08:55:43 -0700

    Refactored matadd demonstration, uses inheritance, flushing out matrix object.

--- hip/matadd/mat.h
+++ hip/matadd/mat.h
@@ -1,45 +1,39 @@
 #ifndef MAT_H
 #define MAT_H
 #include <hip/hip_runtime.h>
+#include <memory>
+#include <string>
+#include <vector>
 
-class Matrix
-{
+class Matrix {
 public:
+  /* Member Data */
+  int width;
+  int height;
+};
 
-/* Member Data */ 
-    int width; 
-    int height; 
-    int my_type; 
-    float* elements; 
+class GpuMatrix : public Matrix {
+public:
+  float *elements;
 
-/* Constructors */ 
-    Matrix(const int w, const int h, const int type = 0){
-        width = w; 
-        height = h;
-        my_type = type; //Matrix knows if it's CPU or GPU 
-        if(type == 0)
-            elements = new float[width*height];
-        else
-            hipMalloc(&elements, width*height*sizeof(float));  
-    }
+  /* Constructor */
+  GpuMatrix(const int w, const int h) {
+    width = w;
+    height = h;
+    hipMalloc(&elements, width * height * sizeof(float));
+  }
 
-/* member functions */ 
-    void load(const Matrix old_matrix, const int dir = 0){
-        size_t size = width*height*sizeof(float);
-        if(dir == 0){ //CPU copy
-            memcpy(elements, old_matrix.elements, size); 
-        }
-        else if(dir == 1){ //GPU copy host to device
-            hipMemcpy(elements, old_matrix.elements, size, hipMemcpyHostToDevice);  
-        }
-    }
+  void deAllocate() { hipFree(elements); }
+};
 
-    void cpu_deallocate(){
-        delete elements; 
-    }
-
-    void gpu_deallocate(){
-        hipFree(elements); //Do not use hipFree 
-    }
+class CpuMatrix : public Matrix {
+public:
+  std::vector<float> elements;
+  /* Constructor */
+  CpuMatrix(const int w, const int h, const float val = 0) {
+    width = w;
+    height = h;
+    elements.resize(w * h, val);
+  }
 };
 #endif
--- hip/matadd/matadd.cpp
+++ hip/matadd/matadd.cpp
@@ -1,71 +1,55 @@
+#include "mat.h"
 #include <iostream>
 #include <stdlib.h>
-#include "mat.h"
 
-/* 
+/*
     Kernel: Adds two matrices and stores them in a third.
     A, B inputs
-    C output 
-*/ 
-__global__ void MatAddKernel(const Matrix A, const Matrix B, Matrix C)
-{
-	int idx = threadIdx.x + blockDim.x*blockIdx.x; //thread in x
-	int idy = threadIdx.y + blockDim.y*blockIdx.y; //thread in y
-	int tid = idx + A.width*idy; // Memory is 1D 
+    C output
+*/
+__global__ void MatAddKernel(const GpuMatrix A, const GpuMatrix B,
+                             GpuMatrix C) {
+  int idx = threadIdx.x + blockDim.x * blockIdx.x; // thread in x
+  int idy = threadIdx.y + blockDim.y * blockIdx.y; // thread in y
+  int tid = idx + A.width * idy;                   // Memory is 1D
 
-	if(idx < A.width && idy < A.height)
-	{
-		C.elements[tid] = A.elements[tid] + B.elements[tid]; 
-	}
+  if (idx < A.width && idy < A.height) {
+    C.elements[tid] = A.elements[tid] + B.elements[tid];
+  }
 }
 
-/* 
+/*
     Driver for launching MatAddKernel
 */
-void MatAdd(const Matrix A, const Matrix B, Matrix C)
-{
-    int Gpu = 1; 
-    //Use Copy Constructor to allocate and copy from host to device
-    int w = A.width, h = A.height; 
-    Matrix d_A(w, h, Gpu);
-    d_A.load(A, Gpu); /*Cuda Memcpy */ 
+void MatAdd(const CpuMatrix &A, const CpuMatrix &B, CpuMatrix &C) {
+  int w = A.width, h = A.height;
 
-    Matrix d_B(w, h, Gpu); 
-    d_B.load(B, Gpu);
+  // GPU memory allocation is handled by constructor
+  GpuMatrix dA(w, h);
+  hipMemcpy(dA.elements, A.elements.data(), w * h * sizeof(float),
+            hipMemcpyHostToDevice);
+  GpuMatrix dB(w, h);
+  hipMemcpy(dB.elements, B.elements.data(), w * h * sizeof(float),
+            hipMemcpyHostToDevice);
+  GpuMatrix dC(w, h);
 
-    Matrix d_C(w, h, Gpu); 
- 
-    dim3 dimBlock(16, 16); 
-    dim3 dimGrid(A.width/dimBlock.x, A.height/dimBlock.y);
-    MatAddKernel<<<dimGrid,dimBlock>>>(d_A, d_B, d_C); 
-    hipMemcpy(C.elements, d_C.elements, C.width*C.height*sizeof(float), hipMemcpyDeviceToHost); 
-
-	//Free Memory
-    d_A.gpu_deallocate();
-    d_B.gpu_deallocate(); 
-    d_C.gpu_deallocate(); 
+  dim3 dimBlock(16, 16);
+  dim3 dimGrid(A.width / dimBlock.x, A.height / dimBlock.y);
+  MatAddKernel<<<dimGrid, dimBlock>>>(dA, dB, dC);
+  hipMemcpy(C.elements.data(), dC.elements, w * h * sizeof(float),
+            hipMemcpyDeviceToHost);
+  dA.deAllocate();
+  dB.deAllocate();
+  dC.deAllocate();
 }
 
+// Main program
+int main() {
+  // Set up matrices
 
-//Main program 
-int main()
-{
-// Set up matrices
-
-    int N = 8192;
-    int M = 8192;
-    Matrix A(M,N), B(M,N), C(M,N); 
-
-	for( int i = 0; i < A.height; i++){
-		for( int j = 0; j < A.width; j++)
-			{
-    			A.elements[i*A.width + j] = 1.0f;
-	    		B.elements[i*B.width + j] = 1.0f;
-			}
-	}
-
-    MatAdd(A,B,C);
-    A.cpu_deallocate();
-    B.cpu_deallocate();
-    C.cpu_deallocate();
+  int N = 4096;
+  int M = 4096;
+  CpuMatrix A(M, N, 1.f), B(M, N, 1.f), C(M, N);
+  MatAdd(A, B, C);
+  std::cout << C.elements[0] << std::endl;
 }
