30789356b1b2a7a5a7ea764ab1214da5dca6f82a
Steven I Reeves <reevessteven15@gmail.com>
2022-09-13 21:16:10 -0700

    Updated Reversal.cpp

--- hip/reversal/reversal.cpp
+++ hip/reversal/reversal.cpp
@@ -1,56 +1,64 @@
+#include <hip/hip_runtime.h>
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-#include <hip/hip_runtime.h>
 
-/* two array reversal functions to illustrate the use of shared memory */ 
+#define PROB_SIZE 64
+/* two array reversal functions to illustrate the use of shared memory */
 
 /* staticReverse uses statically allocated shared memory
    :inputs: array d, length n
-   :outputs: reversed array d */ 
-__global__ void staticReverse(int *d, int n)
-{
-	__shared__ int s[64]; //static shared memory allocation
-	int t = threadIdx.x; 
-	int tr = n - t - 1;
-	if(t < n)
-	{
-		s[t] = d[t]; 
-		__syncthreads(); //None shall pass
-		d[t] = s[tr];
-	}
+   :outputs: reversed array d */
+__global__ void StaticReverse(int d[], int n) {
+  __shared__ int s[PROB_SIZE]; // static shared memory allocation
+  int t = threadIdx.x;
+  int tr = n - t - 1;
+  if (t < n) {
+    s[t] = d[t];
+    __syncthreads(); // None shall pass
+    d[t] = s[tr];
+  }
 }
 
 /* dynamicReverse uses dynamically allocated shared memory
    :inputs: array d, length n
-   :outputs: reversed array d */ 
-__global__ void dynamicReverse(int *d, int n)
-{
-	extern __shared__ int s[]; 
-	int t = threadIdx.x;
-	int tr = n-t-1; 
-	if(t < n)
-	{
-		s[t] = d[t];
-		__syncthreads();
-		d[t] = s[tr];
-	}
+   :outputs: reversed array d */
+__global__ void DynamicReverse(int d[], int n) {
+  extern __shared__ int s[];
+  int t = threadIdx.x;
+  int tr = n - t - 1;
+  if (t < n) {
+    s[t] = d[t];
+    __syncthreads();
+    d[t] = s[tr];
+  }
 }
 
-int main()
-{
-    const int n = 64; 
-    std::vector<int> a(n, 1); 
-    int *d_a;
-    hipMalloc(&d_a, n*sizeof(int)); 
-    
-    hipMemcpy(d_a, a.data(), n*sizeof(int), hipMemcpyHostToDevice); // Transfer to device
-    
-    dynamicReverse<<<1, n, n*sizeof(int)>>>(d_a, n); //grid ,block ,shared
-    
-    hipMemcpy(a.data(),d_a, n*sizeof(int), hipMemcpyDeviceToHost); //bring it back
-    
-    std::cout<<a[0]<<std::endl;
-    hipFree(d_a);
-    return 0;
+int main() {
+  const int n = PROB_SIZE;
+  std::vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    a[i] = i;
+  }
+
+  int *dA;
+  hipMalloc(&dA, n * sizeof(int));
+  hipMemcpy(dA, a.data(), n * sizeof(int),
+            hipMemcpyHostToDevice); // Transfer to device
+
+  StaticReverse<<<1, n>>>(dA, n); // grid ,block
+  hipMemcpy(a.data(), dA, n * sizeof(int),
+            hipMemcpyDeviceToHost); // bring it back
+
+  std::cout << "First Element of a after static reverse" << '\t' << a[0]
+            << std::endl;
+
+  DynamicReverse<<<1, n, n * sizeof(int)>>>(dA, n); // grid ,block ,shared
+  hipMemcpy(a.data(), dA, n * sizeof(int),
+            hipMemcpyDeviceToHost); // bring it back
+
+  std::cout << "First Element of a after dynamic reverse" << '\t' << a[0]
+            << std::endl;
+  hipFree(dA);
+  return 0;
 }
