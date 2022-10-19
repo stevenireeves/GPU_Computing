5cce33f6ebb43a62edc365e080f782a2c4a97358
Steven I Reeves <reevessteven15@gmail.com>
2022-10-19 16:43:40 -0700

    Formatted spmv.

--- hip/sparse/matcompare.cpp
+++ hip/sparse/matcompare.cpp
@@ -12,7 +12,7 @@
 /* Structs for sparse demonstration */
 typedef struct
 {
-    float		 *val; 
+    float            *val; 
     int              *col; 
 	int            *rwptr; 
 	int             nvals; 
@@ -32,16 +32,16 @@ typedef struct
     Input: csrMatrix A, FP32 array x, FP32 array b
     Output: FP32 array b
 */
-__global__ void csr_mat_vec(const csrMatrix A, const float *x, float *b)
+__global__ void CsrMatVec(const csrMatrix A, const float *x, float *b)
 {
 	// Have kernel go over each row this will give step 1
 	int row = blockDim.x*blockIdx.x + threadIdx.x;
 	if(row < A.nrow){
 		float dot = 0.0f; 
-		int row_start =   A.rwptr[row];
-		int row_end = A.rwptr[row + 1]; 
+		int rowStart =   A.rwptr[row];
+		int rowEnd = A.rwptr[row + 1]; 
 		__syncthreads(); 
-		for(int jj = row_start; jj < row_end; jj++)
+		for(int jj = rowStart; jj < rowEnd; jj++)
 		{	
             int colId = A.col[jj]; 
 			dot += A.val[jj] * x[colId]; // Steps 2, 3, and 4
@@ -55,7 +55,7 @@ __global__ void csr_mat_vec(const csrMatrix A, const float *x, float *b)
     Input: csrMatrix A, FP32 array x, FP32 array b
     Output: FP32 array b
 */
-void spmatvec(const csrMatrix A, const float *x, float *b)
+void SPmV(const csrMatrix A, float *x, float *b)
 {
 
 	int colId; 
@@ -73,44 +73,15 @@ void spmatvec(const csrMatrix A, const float *x, float *b)
 	}
 }
 
-/*__global__ void mat_vec(Matrix A, float *x, float *y)
-{
-
-  int bid = blockIdx.x;
-  float y_val = 0.0;
-  float* Asub;
-
-  int row = threadIdx.x;
-  __shared__ float x_shared[BLOCK_SIZE]; 
-
-  for (unsigned int m = 0; m < (A.width / BLOCK_SIZE); ++m) {
-    if (row < BLOCK_SIZE)
-	{
-     	 x_shared[row] = x[row + m*BLOCK_SIZE];
-	}
-	 Asub = A.elements + BLOCK_SIZE*(bid + m*A.width); 
-	 __syncthreads();
-
-
-    for (unsigned int e = 0; e < BLOCK_SIZE; ++e) {
-      if (row < BLOCK_SIZE)
-        y_val += Asub[row + e * A.width] * x_shared[e];
-    	}
-    
-    __syncthreads();
-  }
-
-  y[row + bid*blockDim.x] = y_val; 
-
-}//*/
 
 /*
     Kernel: Computes Dense Matrix Vector Product. 
     Input: Matrix A, FP32 array x, FP32 array y
     Output: FP32 array y
 */
+
 __global__ void
-mat_vec (Matrix A, float *x, float *y)
+MatVec (Matrix A, float *x, float *y)
 {
     int block_row = blockIdx.x;
     int row = threadIdx.x;
@@ -118,41 +89,27 @@ mat_vec (Matrix A, float *x, float *y)
     if (!(tidx < A.height))
        return; // thread outside bounds.
 
-    __shared__ volatile float xsub[BLOCK_SIZE];
+    __shared__ float aSub[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ volatile float xSub[BLOCK_SIZE];
     float yval = 0.0f; 
     for (int block = 0; block < (A.width+BLOCK_SIZE -1)/BLOCK_SIZE ; block++)
     {
         // grab shared local data for operations
-        xsub[row] = x[block * BLOCK_SIZE + row];
+        for (int e = 0; e < BLOCK_SIZE; e++)
+            aSub[row][e] = A.elements[A.height * tidx + block * BLOCK_SIZE + e];
+        xSub[row] = x[block * BLOCK_SIZE + row];
         // sync threads, all are ready now to compute
         __syncthreads ();
 
         // multiply sub matrix and sub vector
         for (int e = 0; e < BLOCK_SIZE; e++)
-                yval +=  A.elements[A.height * tidx + block * BLOCK_SIZE + e]* xsub[e];
+                yval +=  aSub[row][e]* xSub[e];
         __syncthreads ();
     } 
     y[tidx] = yval;
-
 }// shared_mult
 
-/*
-__global__ void mat_vec(Matrix A, float *x, float *y)
-{
-	int tid = threadIdx.x + blockIdx.x*blockDim.x;
-	float dot = 0; 
-	for( int i = 0; i < A.width; i++)
-		dot += A.elements[i + A.width*tid]*x[i];
-	__syncthreads();
-	y[tid] = dot; 
-} //*/
-
-/*
-    Helper function to load data into Matrix A
-    Input: string file, Matrix A
-    Output: N/A
-*/
-void load_denseMatrix(std::string file, Matrix A)
+void loadDenseMatrix(std::string file, Matrix A)
 {
 	std::ifstream f;
 	f.open(file);  
@@ -189,7 +146,6 @@ void csrMatrixCount(std::string file, csrMatrix &X, int numrow, int numcol)
 	}
 	f.close();
 	X.nvals = k; 
-
 }
 
 /*
@@ -197,7 +153,7 @@ void csrMatrixCount(std::string file, csrMatrix &X, int numrow, int numcol)
     Input: string file, csrMatrix X, int numrow, int numcol
     Output: N/A
 */
-void load_csrMatrix(std::string file, csrMatrix X, int numrow, int numcol)
+void loadCsrMatrix(std::string file, csrMatrix X, int numrow, int numcol)
 {
 	float temp; 
 	int  index = 0;
@@ -227,40 +183,40 @@ void load_csrMatrix(std::string file, csrMatrix X, int numrow, int numcol)
 */
 int main()
 {
-    Matrix A, d_A;
-    csrMatrix X, d_X;  
-    float *v, *d_v, *d_v_out, *spv, *d_spv, *d_spv_out; 
+    Matrix A, dA;
+    csrMatrix X, dX;  
+    float *v, *dV, *dVOut, *spv, *dSpv, *dSpvOut; 
     
     A.width  = 1024; 
     A.height = 1024; 
-    d_A.width  = A.width;
-    d_A.height = A.height; 
+    dA.width  = A.width;
+    dA.height = A.height; 
     X.nrow     = A.height;
-    d_X.nrow   = X.nrow; 
+    dX.nrow   = X.nrow; 
     
     A.elements = new float[A.width*A.height];
-    hipMalloc((void**)&d_A.elements,A.width*A.height*sizeof(float));
-    load_denseMatrix("dmatrix.dat", A);
+    hipMalloc((void**)&dA.elements,A.width*A.height*sizeof(float));
+    loadDenseMatrix("dmatrix.dat", A);
     
     csrMatrixCount("dmatrix.dat",X,A.height,A.width);
     X.val   = new float[X.nvals]; 
     X.col   = new int[X.nvals];
     X.rwptr = new int[X.nrow + 1];
-    load_csrMatrix("dmatrix.dat", X, A.height, A.width);
+    loadCsrMatrix("dmatrix.dat", X, A.height, A.width);
     
-    hipMalloc((void**)&d_X.val, X.nvals*sizeof(float)); 
-    hipMalloc((void**)&d_X.rwptr, (X.nrow + 1)*sizeof(int)); 
-    hipMalloc((void**)&d_X.col, X.nvals*sizeof(int));
-    d_X.nvals = X.nvals;
+    hipMalloc((void**)&dX.val, X.nvals*sizeof(float)); 
+    hipMalloc((void**)&dX.rwptr, (X.nrow + 1)*sizeof(int)); 
+    hipMalloc((void**)&dX.col, X.nvals*sizeof(int));
+    dX.nvals = X.nvals;
     
     dim3 dimBlock(BLOCK_SIZE); 
     dim3 dimGrid((A.width + BLOCK_SIZE - 1)/BLOCK_SIZE); 
     v   = new float[A.width];
     spv = new float[A.width];
-    hipMalloc((void**)&d_v,   A.width*sizeof(float)); 
-    hipMalloc((void**)&d_v_out,   A.width*sizeof(float)); 
-    hipMalloc((void**)&d_spv, A.width*sizeof(float)); 
-    hipMalloc((void**)&d_spv_out, A.width*sizeof(float)); 
+    hipMalloc((void**)&dV,   A.width*sizeof(float)); 
+    hipMalloc((void**)&dVOut,   A.width*sizeof(float)); 
+    hipMalloc((void**)&dSpv, A.width*sizeof(float)); 
+    hipMalloc((void**)&dSpvOut, A.width*sizeof(float)); 
     
     for(int i = 0; i < A.width; i++) {
     	v[i] = 1.0f; 
@@ -268,35 +224,35 @@ int main()
     }
     
     //Dense Device Copy	
-    hipMemcpy(d_A.elements,A.elements,A.height*A.width*sizeof(float),hipMemcpyHostToDevice);
-    hipMemcpy(d_v, v, A.width*sizeof(float),hipMemcpyHostToDevice);
+    hipMemcpy(dA.elements,A.elements,A.height*A.width*sizeof(float),hipMemcpyHostToDevice);
+    hipMemcpy(dV, v, A.width*sizeof(float),hipMemcpyHostToDevice);
     
     //Sparse Device Copy
-    hipMemcpy(d_X.val, X.val, X.nvals*sizeof(float), hipMemcpyHostToDevice);
-    hipMemcpy(d_X.rwptr, X.rwptr, (X.nrow + 1)*sizeof(int), hipMemcpyHostToDevice); 
-    hipMemcpy(d_X.col, X.col, X.nvals*sizeof(int), hipMemcpyHostToDevice); 
-    hipMemcpy(d_spv, spv, A.width*sizeof(float),hipMemcpyHostToDevice);
+    hipMemcpy(dX.val, X.val, X.nvals*sizeof(float), hipMemcpyHostToDevice);
+    hipMemcpy(dX.rwptr, X.rwptr, (X.nrow + 1)*sizeof(int), hipMemcpyHostToDevice); 
+    hipMemcpy(dX.col, X.col, X.nvals*sizeof(int), hipMemcpyHostToDevice); 
+    hipMemcpy(dSpv, spv, A.width*sizeof(float),hipMemcpyHostToDevice);
       
     float DenseElapsedTime, SpElapsedTime;
     hipEvent_t start, stop;
     hipEventCreate(&start);
     hipEventCreate(&stop);
     hipEventRecord(start, 0); 
-    mat_vec<<<dimGrid, dimBlock>>>(d_A, d_v, d_v_out);
+    MatVec<<<dimGrid, dimBlock>>>(dA, dV, dVOut);
     hipEventRecord(stop, 0); 
     hipEventSynchronize(stop); 
     hipEventElapsedTime(&DenseElapsedTime, start, stop); 
     hipDeviceSynchronize(); 
-    hipMemcpy(v,d_v_out,A.height*sizeof(float),hipMemcpyDeviceToHost);
+    hipMemcpy(v,dVOut,A.height*sizeof(float),hipMemcpyDeviceToHost);
     
     hipEventRecord(start, 0);
-    csr_mat_vec<<<dimGrid, dimBlock>>>(d_X, d_spv, d_spv_out); 
+    CsrMatVec<<<dimGrid, dimBlock>>>(dX, dSpv, dSpvOut); 
     hipEventRecord(stop, 0); 
     hipEventSynchronize(stop); 
     hipEventElapsedTime(&SpElapsedTime, start, stop); 
     hipEventDestroy(start); 
     hipEventDestroy(stop); 
-    hipMemcpy(spv, d_spv_out, A.height*sizeof(float), hipMemcpyDeviceToHost);
+    hipMemcpy(spv, dSpvOut, A.height*sizeof(float), hipMemcpyDeviceToHost);
     
     std::cout<<" Dense Mv Time = "<< DenseElapsedTime << "ms"<<std::endl;
     std::cout<<" SpMv Time = "<<SpElapsedTime <<"ms"<<std::endl; 
@@ -312,14 +268,14 @@ int main()
    	        return -1; 
         }
     }
-    hipFree(d_A.elements);
-    hipFree(d_X.val);
-    hipFree(d_X.rwptr);
-    hipFree(d_X.col);
-    hipFree(d_spv);
-    hipFree(d_spv_out);
-    hipFree(d_v);
-    hipFree(d_v_out);
+    hipFree(dA.elements);
+    hipFree(dX.val);
+    hipFree(dX.rwptr);
+    hipFree(dX.col);
+    hipFree(dSpv);
+    hipFree(dSpvOut);
+    hipFree(dV);
+    hipFree(dVOut);
     delete A.elements, X.val, X.rwptr, X.col, v, spv;
 }
 
